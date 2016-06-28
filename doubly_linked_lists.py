3e77cc74447c598c955e14bd4200304705b54df9
Nitish Dayal <nitishdayal@outlook.com>
2016-06-28 11:44:34 -0700

    Made changes to a few methods, left comments
    DoublyLinkedList is now functioning, take a look at the changes made and see if it all makes sense.

--- doubly_linked_lists.py
+++ doubly_linked_lists.py
@@ -1,144 +1,235 @@
-class DoublyLinkedList(object):
-	def __init__(self):
-		self.head = None
-		self.tail = None
-
-
-	def Append(self,node):						#this adds node to end of list
-		
-		newnode = Node(node)
-		if (self.head == None):					#means this is empty list, so this is the first node being added
-			self.head = self.tail = newnode		#head, tail and node values are same for first node
-			self.prev = None 					#there are no prev or next values yet, so set prev and next to None
-			self.next = None
-		else:									#this is not the first node, so there are already head and tail values		
-			newnode.prev = self.tail 			#set current node value to new node									
-			self.tail.next = newnode 			#set previous node's next link to new node
-			
-			self.tail = newnode 				#set new tail to current node
-			self.next = None 					#set new node next link to none
-
-  		return self
-	
-	def Insert(self,newnode,position,targetnode):	#this inserts node either before or after a target node
-		
-		current = self.head 						#start at head node
-		while current != None:
-			if current.value == targetnode:			#look for node matching target node
-				if position == "before":			#insert a new node before a target node
-					newnext = current				#get the current node (which is the target node)
-					newprev = current.prev 			#get the current node's previous node
-					newnode = Node(newnode,newprev,newnext)	#create new node, passing values for prev and next links
-					current.prev.next = newnode 	#reset the previous node's next link to the new node
-					current.prev = newnode 			#reset the current node's prev to the new node
-
-				else:								#insert a new node after a target node
-					newnext = current				#get the current node (which is the target node)
-					newprev = current 				#get the current node, which will be the new node's prev
-					newnext = current.next 			#get the current node's next, which will be the new node's next
-					newnode = Node(newnode,newprev,newnext)	#create new node, passing values for prev and next links
-					current.next.prev = newnode 	#reset current node's next node's prev to the new node
-					current.next = newnode			#reset the current node's next to the new node
-				
-				return True
-		  	current = current.next 					#get the next node
-		print "**************OOPS, target value (",targetnode,") not found!************"
-		return False
-
-
-	def Del(self,node):
-		current = self.head 						#start at the head node
-		while current != None:
-		 	if current.value == node:				#look for node matching node to delete
-				current.prev.next = current.next 	#reset current node's previous node's next link to current node's next node
-				current.next.prev = current.prev 	#reset current node's next node's prev link to current node's previous node
-		 		return True
-		  	current = current.next
-		return False
-
-
-	def PrintAll(self):
-		current = self.head
-		while current != None:
-			print "Node:",current.value
-			if current.prev != None:
-				print "Node.prev:", current.prev.value
-			else:
-				print "Node.prev:", current.prev
-
-
-			if current.next != None:
-				print "Node.next:",current.next.value,"\n"
-			else:
-				print "Node.next:",current.next,"\n"
-			
-			current = current.next
-		return True
-
-	
-	def PrintAll2(self):
-		current = self.head
-		print "Prev -- Node -- Next"
-		while current != None:
-			#print current.value
-			if current.prev != None:
-				print current.prev.value,"--",current.value,"--",
-			else:
-				print current.prev,"--",current.value,"--",
-
-			if current.next != None:
-				print current.next.value,"\n"
-			else:
-				print current.next,"\n"
-			
-			current = current.next
-		return True
-  
+'''
+Create Node class, which takes a value upon initalization and sets it to
+Node.value. Node.next and Node.previous are none at time of instantiation.
+'''
 
 
 class Node(object):
-	def __init__(self,node,prev=None,next=None):
-		self.value = node
-		self.next = next		 
-		self.prev = prev		
-		
+
+    def __init__(self, value, prev=None, next=None):
+        self.value = value
+        self.prev = prev
+        self.next = next
+
+'''
+Create DoublyLinkedList class, takes no arguments upon initialization and sets
+DoublyLinkedList.head and DoublyLinkedLIst.tail to none.
+'''
 
 
-#instantiate the list
+class DoublyLinkedList(object):
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def Append(self, value):  # this adds node to end of list
+        newnode = Node(value)
+        if self.head is None:
+            # means this is empty list, so this is the first node being added
+            # head, tail and node values are same for first node
+            self.head = self.tail = newnode
+        else:  # this is not the first node, so there are already head and tail values
+            newnode.prev = self.tail  # set current node value to new node
+            self.tail.next = newnode  # set previous node's next link to new node
+
+            self.tail = newnode  # set new tail to current node
+            self.tail.next = None  # set new node next link to none
+        return self
+
+    # this inserts node either before or after a target node
+    def Insert(self, value, position, targetvalue):
+        current = self.head  # start at head node
+        while current is not None:
+            # look for node containing value which equal targetvalue
+            if current.value == targetvalue:
+                if position == "before":  # insert a new node before a target node
+                    # create new node, passing current.prev Node as 'prev' and current
+                    # node as 'next'
+                    newnode = Node(value, current.prev, current)
+                    if current == self.head:
+                        current.prev = self.head = newnode
+                    else:
+                        current.prev.next = current.prev = newnode
+
+                else:  # insert a new node after a target node
+                    # create new node, passing current node as 'prev' and current.next
+                    # node as 'previous'
+                    newnode = Node(value, current, current.next)
+                    if current == self.tail:
+                        current.next = self.tail = newnode
+                        newnode.prev = current
+                    else:
+                        current.next = current.next.prev = newnode
+                        newnode.prev = current
+
+                return "Successfully inserted new Node with value {0} {1} Node containing the value {2}.".format(value, position, targetvalue)
+            current = current.next  # get the next node
+        print "**************OOPS, target value (", targetvalue, ") not found!************"
+        return False
+
+    def Del(self, value):
+        current = self.head  # start at the head node
+        while current is not None:
+            if current.value == value:  # look for node which has a value equal to value provided
+                # Handle case if current Node is the Tail node in our list
+                if current is self.tail:
+                    current = current.prev
+                    current.next = None
+                    self.tail = current
+                # Handle case if current node is Head node in our list
+                elif current is self.head:
+                    current = current.next
+                    current.prev = None
+                    self.head = current
+                # Else in middle of the list, proceed to adjust pointers
+                else:
+                    current.prev.next = current.next
+                    current.next.prev = current.prev
+                    current.next = None
+                    current.prev = None
+                print(
+                    "Node containining value {0} was removed from "
+                    "DoublyLinkedList"
+                    .format(value))
+                return "Node found and removed from DoublyLinkedList"
+            current = current.next
+        return False
+
+    def PrintAll(self):
+        current = self.head
+        nodecount = 1  # Starting count at 1
+        nodevals = []
+        if current is None:
+            return "\n!!!!!!~~~~~ No Nodes in Doubly linked list! Please insert " \
+                "Nodes containing values! !!!!!!~~~~~\n\n"
+        print "!!!!!!~~~~~~BEGIN DOUBLY LINKED LIST INFORMATION ~~~ ~~~!!!!!!\n"
+        while current is not None:
+            nodeinfostring = "Node at position {0} has a value of {1}".format(
+                nodecount, current.value)
+            if nodecount == 1:
+                nodeinfostring = "Head " + nodeinfostring
+            nodevals.append(current.value)
+            current = current.next
+            if current is not None:
+                nodecount += 1
+                nodeinfostring = nodeinfostring + " and has a next Node."
+            else:
+                nodeinfostring = "Tail " + nodeinfostring + "."
+            print nodeinfostring
+        print "\n\n---Total List Length: {}\n" \
+            "---Values: {}\n".format(
+                nodecount, nodevals)
+        return "~~~~~~ END DOUBLY LINKED LIST INFORMATION...for now...~~~~~~\n"
+
+    # def PrintAll2(self):
+    #     current = self.head
+    #     print "Prev -- Node -- Next"
+    #     while current is not None:
+    #         # print current.value
+    #         if current.prev is not None:
+    #             print current.prev.value, "--", current.value, "--",
+    #         else:
+    #             print current.prev, "--", current.value, "--",
+
+    #         if current.next is not None:
+    #             print current.next.value, "\n"
+    #         else:
+    #             print current.next, "\n"
+
+    #         current = current.next
+    #     return True
+
+
+# instantiate the list
 dlList = DoublyLinkedList()
-#dlList.NodeInfo()
 
-#add values to list
-dlList.Append(23)
+
+# Call on PrintAll function to verify that our DoublyLinkedList exists and has
+# no values
 # dlList.NodeInfo()
+print dlList.PrintAll()
+
+# add values to list
+dlList.Append(5)
 
 
-dlList.Append(25)
-#dlList.NodeInfo()
+# Validate
+print dlList.PrintAll()
 
 
-dlList.Append(39)
-#dlList.NodeInfo()
+# Call on Append function, providing a value to insert into a new node, and
+# then insert that Node at the end of the DoublyLinkedList
+dlList.Append(7)
 
 
-dlList.Append(47)
-#dlList.NodeInfo()
+# Validate
+print dlList.PrintAll()
 
-print "\n****************Doubly linked list with values****************"
-dlList.PrintAll()
 
-print "\n*************************Delete node 39***********************"
-dlList.Del(39)
+# Call on Append function, providing a value to insert into a new node, and
+# then insert that Node at the end of the DoublyLinkedList
+dlList.Append(8)
 
-dlList.PrintAll()
 
-print "\n***********************Insert 99 before 39********************"
-dlList.Insert(99,"before",39)
-dlList.PrintAll()
+# Validate
+print dlList.PrintAll()
 
-print "\n************************Insert 88 after 25********************"
-dlList.Insert(88,"after",25)
-dlList.PrintAll()
 
-print "\n*******************Print list with different format************"
-dlList.PrintAll2()
+# Call on Append function, providing a value to insert into a new node, and
+# then insert that Node at the end of the DoublyLinkedList
+dlList.Append(3)
+
+# Validate
+print dlList.PrintAll()
+
+# Call on Append function, providing a value to insert into a new node, and
+# then insert that Node at the end of the DoublyLinkedList
+dlList.Append(9)
+
+# Validate
+print dlList.PrintAll()
+
+# Call on Append function, providing a value to insert into a new node, and
+# then insert that Node at the end of the DoublyLinkedList
+dlList.Append(14)
+
+# Validate
+print dlList.PrintAll()
+
+# Delete node with value 5 (AKA testing for head node deletion success)
+dlList.Del(5)
+
+
+# Validate
+print dlList.PrintAll()
+
+
+# Delete node with value 3 (AKA testing for middle node deletion sucess)
+dlList.Del(3)
+
+
+# Validate
+print dlList.PrintAll()
+
+# Delete node with value 3 (AKA testing for middle node deletion sucess)
+dlList.Del(14)
+
+
+# Validate
+print dlList.PrintAll()
+
+# Insert after test
+print dlList.Insert(22, "before", 9)
+
+# Insert after test, tail
+print dlList.Insert(33, "before", 7)
+
+# Insert before test
+print dlList.Insert(10, "after", 9)
+
+# Insert before test, head
+print dlList.Insert(1, "after", 33)
+
+# Validate
+print dlList.PrintAll()
