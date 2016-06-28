32f509e6b9d896459497e44bf2265b09bf2d6947
Nitish Dayal <nitishdayal@outlook.com>
2016-06-28 11:45:29 -0700

    Updated to underscore.py
    Minor changes, commented to explain reasons. Please contact via Slack with any questions.

--- underscore.py
+++ underscore.py
@@ -1,55 +1,65 @@
 
 class Underscore(object):
-	def map(self,arr,func):
-		newList = []										#initialize new list
-		for val in arr:										#iterate elements in passed list
-			newList.append(func(val))						#apply passed function to elements and append to new list
-		return newList
+
+    def map(self, arr, func):
+        newList = []  # initialize new list
+        for val in arr:  # iterate elements in passed list
+            # apply passed function to elements and append to new list
+            newList.append(func(val))
+        return newList
+
+        # Reduce should take an array and a callback
+        # and apply the provided callback to every value in
+        # the array, returning a single value.
+    def reduce(self, arr, func):
+        # initialize int variable
+        running_total = 0
+        for i in arr:
+            # iterate through list item and get value as 'i'
+            # pass each value into provided lamba function as well as the
+            # current running total
+            running_total = func(i, running_total)
+        return running_total  # Return running total
+
+    def find(self, arr, func):
+        for val in arr:  # iterate passed list
+            # If return value from lambda is 0, we have the first
+            # even number
+            if func(val) == 0:
+                # Return even number as well as index of even number in array
+                return "Found at index {0}, {1}".format(arr.index(val), val)
+
+    def filter(self, arr, func):
+        newList = []  # initialize new list
+        for val in arr:
+            # append to new list each element that meets condition in passed
+            # function
+            newList.append(val) if func(val) else None
+            # ^ Single line statement that reads like a sentence. Awesome syntax.
+        return newList
+
+    def reject(self, arr, func):
+        newList = []  # initialize new list
+        for val in arr:
+            # append to new list each element that fails condition in passed
+            # function
+            newList.append(val) if not func(val) else None
+        return newList
 
 
-	def reduce(self,arr,func):
-		val = arr[0]										#get the value of first element of passed array
-	 	for ndx in range(1,len(arr)):						#iterate passed list
-	 		val = func(val,arr[ndx])						#pass current value of val and array element to passed function
-	 	return val
+_ = Underscore()  # instantiate Underscore class
 
+myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-	def find(self,arr,func):								
-		for val in arr:										#iterate passed list
-			if func(val):									#return the first instance that meeds condition in passed function
-				return [val]			
+# call methods of Underscore class & pass in list and lambda; print returned
+# list. Only pass in lambda functions as the callback
 
+print _.map(myList, lambda x: x + 5)
 
-	def filter(self,arr,func):
-		newList=[]											#initialize new list
-		for val in arr:										
-			newList.append(val) if func(val) else None		#append to new list each element that meets condition in passed function
-		return newList
+print _.reduce(myList, lambda x, y: x + y)
 
+print _.find(myList, lambda x: x % 2)
 
-	def reject(self,arr,func):
-		newList=[]											#initialize new list
-		for val in arr:										
-			newList.append(val) if not func(val) else None		#append to new list each element that fails condition in passed function
-		return newList
+print _.filter(myList, lambda x: x % 2 == 0)
 
-
-_ = Underscore() # instantiate Underscore class
-
-myList = [1,2,3,4,5]
-
-#call methods of Underscore class, passing in list and function; print returned list
-#the passed function can be lambda or a previously defined named function
-
-print _.map(myList,lambda x: x+5)
-
-print _.reduce(myList,lambda x,y: x*y)
-
-print _.find(myList,lambda x: x==4)
-
-print _.filter(myList,lambda x: x>=3)
-
-print _.reject(myList,lambda x: x>=3)
-
-
-
+print _.reject(myList, lambda x: x >= 3)
